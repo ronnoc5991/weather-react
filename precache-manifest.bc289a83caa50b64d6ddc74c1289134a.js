47ca5c66c8a429a58f12cbff98c44718211a32e5
Connor <cstreng95@gmail.com>
2020-07-08 17:39:16 +0200

    Updates

---
+++ precache-manifest.bc289a83caa50b64d6ddc74c1289134a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c91fdc4892c442b4e7341d35a636ed",
+    "url": "/weather-react/index.html"
+  },
+  {
+    "revision": "afabaf837725c92b4274",
+    "url": "/weather-react/static/css/main.cb9f1f9f.chunk.css"
+  },
+  {
+    "revision": "f67fa0e9dd85303e9fe1",
+    "url": "/weather-react/static/js/2.f9d7b3b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react/static/js/2.f9d7b3b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afabaf837725c92b4274",
+    "url": "/weather-react/static/js/main.b21d4a94.chunk.js"
+  },
+  {
+    "revision": "66a38fe6e1e50064e97c",
+    "url": "/weather-react/static/js/runtime-main.4f845094.js"
+  },
+  {
+    "revision": "75a0b164132c4da2910306e30e5e504c",
+    "url": "/weather-react/static/media/mountains.75a0b164.jpg"
+  }
+]);
